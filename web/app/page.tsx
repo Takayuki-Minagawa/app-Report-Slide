be2a8b1f58381561570058c7c0ef699393079011
皆川隆之 <minagawa@ebi-ken.co.jp>
2026-09-03 07:10:08 +0900

    ci: deploy app to GitHub Pages

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,5 +1,7 @@
 import { EditorWorkspace } from '@/components/editor/editor-workspace';
 
+export const dynamic = 'force-static';
+
 export default function Home() {
   return <EditorWorkspace />;
 }
